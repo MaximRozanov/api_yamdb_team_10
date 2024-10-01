71e4a8a089532f0bba20499ed91568443bb2be80
Maxim Rozanov <m.rozanoov@gmail.com>
2024-10-01 23:24:04 +0300

    model User, serializers, begin View

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,16 @@
 from rest_framework.routers import SimpleRouter
 from django.urls import include, path
 
-from api.views import CategoryViewSet, GenreViewSet
+from api.views import CategoryViewSet, GenreViewSet, UsersViewSet
 
 router_v1 = SimpleRouter() 
 router_v1.register('categories', CategoryViewSet, basename='category')
 router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('users', UsersViewSet, basename='users')
 
-urlpatterns = [path('', include(router_v1.urls)), ]
+urlpatterns = [
+
+    path('', include(router_v1.urls)),
+
+
+]
