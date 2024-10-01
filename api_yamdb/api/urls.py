78044e318d9613ecc3bd1ad9cb78786322e3abbb
Nikita Prokhvatilov <IvensGin@yandex.ru>
2024-10-02 02:44:04 +0300

    Заменён Simplerouter на DefaultRouter и изменено формирование пути.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,11 @@
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 
 from api.views import CategoryViewSet, GenreViewSet
+from api.constants import VERSION
 
-router_v1 = SimpleRouter() 
+router_v1 = DefaultRouter()
 router_v1.register('categories', CategoryViewSet, basename='category')
 router_v1.register('genres', GenreViewSet, basename='genre')
 
-urlpatterns = [path('', include(router_v1.urls)), ]
+urlpatterns = [path(f'api/{VERSION}/', include(router_v1.urls)), ]
