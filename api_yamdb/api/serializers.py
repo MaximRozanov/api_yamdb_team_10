3d2318fa31ef1181a5526c26a1595399dbaa0457
Nikita Prokhvatilov <IvensGin@yandex.ru>
2024-10-07 21:11:09 +0300

    Pep8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -14,13 +14,23 @@ from reviews.models import Category, Title, Genre, User, Review, Comment
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
+        fields = ('username',
+                  'email',
+                  'first_name',
+                  'last_name',
+                  'bio',
+                  'role')
 
 
 class NoAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
+        fields = ('username',
+                  'email',
+                  'first_name',
+                  'last_name',
+                  'bio',
+                  'role')
         read_only_fields = ('role',)
 
 
